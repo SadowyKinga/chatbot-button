3cef692d2a27325f554864519d3b63d727742df9
Kinga Sadowy <ksadowy@betasi.pl>
2021-11-15 14:18:18 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+
+function App() {
+  return (
+    <div>
+      <button className="hidden-button">
+      <div className="click-chat" style={{ position: "relative" }}>
+        <ChatBubbleOutlineIcon />
+        <div className="dots-cont">
+          <span className="dot dot-1"></span>
+          <span className="dot dot-2"></span>
+          <span className="dot dot-3"></span>
+        </div>
+      </div>
+    </button>
+    </div >
+   
+  );
+}
+
+export default App;
